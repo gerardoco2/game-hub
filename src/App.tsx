0ee7ff0b1cf7ee95678b35572b424d51516b21b1
Gerardo <gerardocolina@gmail.com>
2024-06-17 07:15:22 -0400

    Build a responsive layaout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="large">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
 
-
-  return  <Button colorScheme='blue'>Button</Button>;
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
